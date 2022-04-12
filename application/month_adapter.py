9dca55570185d32056704867b35fe96cc4ce80f3
GALJO <jasiek.ole@gmail.com>
2022-04-12 13:11:24 +0200

    endpoint "month/days-left" implemented TODO docs

--- application/month_adapter.py
+++ application/month_adapter.py
@@ -1,6 +1,8 @@
 from app import app
 from domain.date import month_service
 
-@app.route("/month/days-left")
+
+@app.route("/month/days-left", methods=["GET"])
 def month_days_left():
-    return {"daysLeft": month_service.get_days_left()}
+    # TODO docs
+    return {"days-left": month_service.get_days_left()}
--- domain/date/month_service.py
+++ domain/date/month_service.py
@@ -1,3 +1,11 @@
+from calendar import monthrange
+from datetime import datetime
+
+
 def get_days_left():
-    # TODO: calculate how many days left to the end of the month
-    return 0;
+    # TODO docs
+    _act_month = datetime.utcnow().month
+    _act_year = datetime.utcnow().year
+    _act_day = datetime.utcnow().day
+    _days_in_month = monthrange(_act_year, _act_month)[1]
+    return _days_in_month - _act_day
