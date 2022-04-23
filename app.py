ec6ef02fba4a746b5f18839eda72116e6a5ab0a9
Gerard Olenski <gerard.olenski@gmail.com>
2022-04-23 22:15:15 +0200

    Environment init improvements

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import logging
 
 from flask import Flask
 
+from infrastructure.config.environ_config import import_all_envs
 from infrastructure.config.ports_config import AppPorts
 from infrastructure.logging.logging_formatter import CustomFormatter
 
@@ -18,8 +19,11 @@ logger.setLevel(logging.DEBUG)
 logger.addHandler(ch)
 logging.info("Activated logging")
 
+# envs
+import_all_envs(app.config)
+
 # application ports configuration
-ports = AppPorts(app)
+ports = AppPorts(app.config)
 logging.info("Configured app ports")
 
 from application import year_adapter
--- infrastructure/config/ports_config.py
+++ infrastructure/config/ports_config.py
@@ -1,42 +1,34 @@
 import logging
-from os import environ
 
-from domain.car.car_modify_port import CarModifyPort
-from domain.car.car_query_port import CarQueryPort
+from flask import Config
+
 from domain.car.adapter.dummy.dummy_car_modify_port import DummyCarModifyPort
 from domain.car.adapter.dummy.dummy_car_query_port import DummyCarQueryPort
 from domain.car.adapter.in_memory.in_memory_car_modify_port import InMemoryCarModifyPort
 from domain.car.adapter.in_memory.in_memory_car_query_port import InMemoryCarQueryPort
+from domain.car.car_modify_port import CarModifyPort
+from domain.car.car_query_port import CarQueryPort
 
 
 # This is global application ports configuration
 
 class AppPorts:
-    def __init__(self, _app) -> None:
-        self.car_modify_port, self.car_query_port = config_car_module(_app)
+    def __init__(self, _config: Config) -> None:
+        self.car_modify_port, self.car_query_port = config_car_module(_config)
 
 
-def config_car_module(_app) -> [CarModifyPort, CarQueryPort]:
+def config_car_module(_config: Config) -> [CarModifyPort, CarQueryPort]:
     """
     Prepares configuration of ports in CAR module
     """
     logging.info("Configuring car ports")
-
-    if environ.get("CAR_PORT") is None:
-        logging.critical("ABORTING INIT - CAR_PORT environment variable not given")
-        exit(1)
-    else:
-        _app.config.update({"CAR_PORT": environ.get("CAR_PORT")})
-        logging.debug("Variable CAR_PORT has been saved in app config")
-
-    if _app.config["CAR_PORT"] == "DUMMY":
-        # DUMMY configuration
-        logging.info("Configuring DUMMY car ports")
-        return DummyCarModifyPort(), DummyCarQueryPort()
-    elif _app.config["CAR_PORT"] == "IN_MEMORY":
-        # IN MEMORY configuration
-        logging.info("Configuring IN MEMORY car ports")
-        return InMemoryCarModifyPort(), InMemoryCarQueryPort()
-    else:
-        logging.critical("ABORTING INIT - CAR_PORT environment variable is not valid with any existing car port")
-        exit(1)
+    match _config.get('TEASER_CAR_PORT'):
+        case "DUMMY":
+            logging.info("Configuring DUMMY car ports")
+            return DummyCarModifyPort(), DummyCarQueryPort()
+        case "IN_MEMORY":
+            logging.info("Configuring IN MEMORY car ports")
+            return InMemoryCarModifyPort(), InMemoryCarQueryPort()
+        case _:
+            logging.critical("ABORTING INIT - unknown TEASER_CAR_PORT environment variable")
+            exit(1)
